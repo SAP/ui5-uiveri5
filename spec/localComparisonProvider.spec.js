b4b5b6c61cb446bf87c88ddedfbbdd4f7063ed2f
Hristo Manchev <hristo.manchev@sap.com>
2015-08-20 13:34:39 +0300

    Local comparison provider test spec

---
+++ spec/localComparisonProvider.spec.js
@@ -0,0 +1,58 @@
+/**
+ * Created by I304310 on 6/15/2015.
+ */
+
+var fs = require('fs');
+require('jasminewd2');
+
+describe("RemoteSAPUI5SpecResolver", function () {
+  beforeEach(function () {
+
+    var storageProviderMock = {
+      storeRefImage : function(refImageName) {
+        return fs.createWriteStream(refImageName);
+      },
+      readRefImage: function(refImageName) {
+        return fs.createReadStream(refImageName);
+      },
+      storeDiffImage : function() {
+        return fs.createWriteStream('spec/localComparisonProvider/diff.png');
+      },
+      storeActImage : function() {
+        return fs.createWriteStream('spec/localComparisonProvider/act.png');
+      }
+    };
+
+    var config = {
+      tolerance: 10,
+      compare: true,
+      localComparisonProvider: {
+        update: false,
+        ignoreColors: false
+      }
+    };
+
+    var comparison = require('../src/localComparisonProvider.js')(
+      config,
+      storageProviderMock
+    );
+
+    comparison.register();
+  });
+
+  it('Should have mismatch percentage less than 10%.', function () {
+    var imageFile = fs.readFileSync('spec/localComparisonProvider/People.png').toString('base64');
+    expect(imageFile).toLookAs('spec/localComparisonProvider/People2.png');
+
+  });
+
+  it('Should have mismatch percentage less than 10%.', function() {
+    var imageFile = fs.readFileSync('spec/localComparisonProvider/People2.png').toString('base64');
+    expect(imageFile).toLookAs('spec/localComparisonProvider/People.png');
+  });
+
+  it('Should not look like the ref image - mismatch percentage more than 10%..', function() {
+    var imageFile = fs.readFileSync('spec/localComparisonProvider/example.png').toString('base64');
+    expect(imageFile).not.toLookAs('spec/localComparisonProvider/People.png');
+  })
+});

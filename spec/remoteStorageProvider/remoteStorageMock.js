051c742213195601981b7f51d6c68a3d4d7327a6
Hristo Manchev <hristo.manchev@sap.com>
2015-12-02 14:03:44 +0200

    fix: removed json file upload in remoteStorageMock

--- spec/remoteStorageProvider/remoteStorageMock.js
+++ spec/remoteStorageProvider/remoteStorageMock.js
@@ -1,25 +1,10 @@
+
 var express = require('express');
-var fs = require('fs');
 var multer = require('multer');
 var upload = multer({});
 
 var app = express();
-var dataImage = [];
-var imagePath = './localComparisonProvider/Different.png';
-
 var imagesMap = {};
-fs.createReadStream(imagePath)
-  .on('error', function(error) {
-    rejectFn(error);
-  })
-  .on('data', function(chunk) {
-    dataImage.push(chunk);
-  })
-  .on('end', function() {
-     imagesMap['qwerty'] = Buffer.concat(dataImage);
-});
-
-imagesMap['asdf'] = '1234';
 
 function getImage(uid) {
   return imagesMap[uid];
@@ -27,7 +12,13 @@ function getImage(uid) {
 
 // List all existing image's uuid's
 app.get('/images/', function(req, res) {
-  res.send(imagesMap);
+  var uuids = [];
+
+  for(var key in imagesMap) {
+    uuids.push(key);
+  }
+
+  res.send(uuids);
 });
 
 // Get the image with given uuid
@@ -43,19 +34,17 @@ app.delete('/images/:uuid', function(req, res) {
   res.send('Image deleted.');
 });
 
-var uploads = upload.fields([{ name: 'image', maxCount: 1 }, { name: 'json', maxCount: 1 }])
+var uploads = upload.single('image')
 
 // Save image from given multipart file and json file
 app.post('/images', uploads, function(req, res) {
-  var files = req.files;
-  var imageBuffer = new Buffer(files['image'][0].buffer);
-  var jsonBuffer = new Buffer(files['json'][0].buffer);
   var uuid = Math.floor((1 + Math.random()) * 0x10000);
+  var file = req.file;
+  var imageBuffer = new Buffer(file.buffer);
 
   imagesMap[uuid] = {};
 
-  imagesMap[uuid]['image'] = imageBuffer;
-  imagesMap[uuid]['json'] = jsonBuffer;
+  imagesMap[uuid] = imageBuffer;
 
   var jsonRes = {
     "uuid" : uuid

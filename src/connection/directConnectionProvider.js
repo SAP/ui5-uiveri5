37094235dfd7f00425b96e3a845c696c4d73c016
Tsvetelina Aleksandrova <tsvetelina.aleksandrova@sap.com>
2019-10-11 14:51:49 +0300

    fix: skip repeated driver download (#100)
    * fix: skip repeated driver download

--- src/connection/directConnectionProvider.js
+++ src/connection/directConnectionProvider.js
@@ -308,10 +308,17 @@ DirectConnectionProvider.prototype._downloadBinary = function (binary) {
   } else {
     binary.executable =  root + '/' + binary.executable;
   }
+  binary.executable = path.resolve(binary.executable);
 
   return that._getLatestVersion(binary).then(function () {
-    return that._checkIfBinaryExists(binary.executable, true).then(function () {
-      return that._downloadDriver(binary);
+    return that._checkIfBinaryExists(binary.executable, true).then(function (pathToExistingBinary) {
+      if (pathToExistingBinary) {
+        var deferred = q.defer();
+        deferred.resolve(pathToExistingBinary);
+        return deferred.promise;
+      } else {
+        return that._downloadDriver(binary);
+      }
     });
   });
 };

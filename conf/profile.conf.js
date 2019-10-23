4c24b118e366e5d59e9950556c716fd878524e7b
DenisDuev <denis_duev@abv.bg>
2019-10-23 15:18:59 +0300

    fix formatting

--- conf/profile.conf.js
+++ conf/profile.conf.js
@@ -166,7 +166,7 @@ exports.config = {
         }
       }
     }
-
+    
     /* WARNING: ignoring protected mode may introduce errors
      * A better solution would be to configure your IE browser:
      * https://github.com/seleniumQuery/seleniumQuery/wiki/seleniumQuery-and-IE-Driver#protected-mode-exception-while-launching-ie-driver

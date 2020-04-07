ef911f1e31fc39c520da6bb87ebc770cd9960259
Maximilian Lenkeit <maximilian.lenkeit@gmail.com>
2020-04-07 11:26:49 +0200

    fix: do not overwrite capabilities from custom config

--- src/connection/directConnectionProvider.js
+++ src/connection/directConnectionProvider.js
@@ -71,22 +71,25 @@ DirectConnectionProvider.prototype.resolveCapabilitiesFromRuntime = function(run
   // save this runtime so setupEnv() could download respective drivers
   this.runtimes.push(runtime);
 
-  var capabilities = {};
+  // default capabilities with capabilities provided in the config already
+  var capabilities = Object.assign({}, runtime.capabilities || {});
 
   // capabilities according:
   // https://github.com/SeleniumHQ/selenium/wiki/DesiredCapabilities
   // http://appium.io/slate/en/master/?ruby#appium-server-capabilities
 
   // format browserName
-  if (runtime.platformName === 'android' || runtime.platformName === 'ios') {
-    capabilities.browserName = runtime.browserName.charAt(0).toUpperCase() + runtime.browserName.slice(1);
-  } else {
-    if (runtime.browserName === 'ie') {
-      capabilities.browserName = 'internet explorer';
-    } else if (runtime.browserName === 'edge') {
-      capabilities.browserName = 'MicrosoftEdge';
+  if (!capabilities.browserName) {
+    if (runtime.platformName === 'android' || runtime.platformName === 'ios') {
+      capabilities.browserName = runtime.browserName.charAt(0).toUpperCase() + runtime.browserName.slice(1);
     } else {
-      capabilities.browserName = runtime.browserName;
+      if (runtime.browserName === 'ie') {
+        capabilities.browserName = 'internet explorer';
+      } else if (runtime.browserName === 'edge') {
+        capabilities.browserName = 'MicrosoftEdge';
+      } else {
+        capabilities.browserName = runtime.browserName;
+      }
     }
   }
 
@@ -96,39 +99,41 @@ DirectConnectionProvider.prototype.resolveCapabilitiesFromRuntime = function(run
   }
 
   // format platformName
-  if (runtime.platformName === 'windows') {
-    if (runtime.platformVersion === '*') {
-      capabilities.platform = 'WINDOWS';
-    } else if (runtime.platformVersion === 'XP') {
-      capabilities.platform = 'XP';
-    } else if (runtime.platformVersion === 'VISTA' || runtime.platformVersion === '7') {
-      capabilities.platform = 'VISTA';
-    } else if (runtime.platformVersion === '8') {
-      capabilities.platform = 'WIN8';
-    } else if (runtime.platformVersion === '8.1') {
-      capabilities.platform = 'WIN8_1';
+  if (!capabilities.platform && !capabilities.platformName) {
+    if (runtime.platformName === 'windows') {
+      if (runtime.platformVersion === '*') {
+        capabilities.platform = 'WINDOWS';
+      } else if (runtime.platformVersion === 'XP') {
+        capabilities.platform = 'XP';
+      } else if (runtime.platformVersion === 'VISTA' || runtime.platformVersion === '7') {
+        capabilities.platform = 'VISTA';
+      } else if (runtime.platformVersion === '8') {
+        capabilities.platform = 'WIN8';
+      } else if (runtime.platformVersion === '8.1') {
+        capabilities.platform = 'WIN8_1';
+      } else {
+        throw Error('Platform version: ' + runtime.platformVersion +
+        ' for platformName: WINDOWS is not supported by directConnectionProvider');
+      }
+    } else if (runtime.platformName === 'linux' || runtime.platformName === 'mac') {
+      capabilities.platform = runtime.platformName.toUpperCase();
+    } else if (runtime.platformName === 'ios') {
+      capabilities.platformName = 'iOS';
+    } else if (runtime.platformName === 'android') {
+      capabilities.platformName = 'Android';
     } else {
-      throw Error('Platform version: ' + runtime.platformVersion +
-      ' for platformName: WINDOWS is not supported by directConnectionProvider');
+      throw Error('Platform name: ' + runtime.platformName +	
+        ' not supported by directConnectionProvider');	
     }
-  } else if (runtime.platformName === 'linux' || runtime.platformName === 'mac') {
-    capabilities.platform = runtime.platformName.toUpperCase();
-  } else if (runtime.platformName === 'ios') {
-    capabilities.platformName = 'iOS';
-  } else if (runtime.platformName === 'android') {
-    capabilities.platformName = 'Android';
-  } else {
-    throw Error('Platform name: ' + runtime.platformName +	
-      ' not supported by directConnectionProvider');	
   }
 
   // format platformVersion
-  if (runtime.platformVersion !== '*'){
+  if (!capabilities.platformVersion && runtime.platformVersion !== '*'){
     capabilities.platformVersion = runtime.platformVersion;
   }
 
   // format deviceName
-  if (runtime.deviceName !== '*'){
+  if (!capabilities.deviceName && runtime.deviceName !== '*'){
     capabilities.deviceName = runtime.deviceName;
   }
 
